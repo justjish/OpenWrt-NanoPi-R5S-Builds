61feacfe4929db1ea92416db8ab4784739f54135
Sujish K Patel <justjish@gmail.com>
2023-11-17 11:31:28 -0500

    repo clone update

--- openwrt-main/steps/01_clone_openwrt.sh
+++ openwrt-main/steps/01_clone_openwrt.sh
@@ -12,9 +12,8 @@ cd build
 
 OPENWRT_FOLDER=openwrt-fresh-main
 IMMORTALWRT_FOLDER=$ROOTDIR/immortalwrt-fresh-main # Alias master for easy integration
-git clone -b openwrt-main --single-branch https://git.openwrt.org/openwrt/openwrt.git $OPENWRT_FOLDER
-git clone -b immortalwrt-main --single-branch https://git.openwrt.org/immortalwrt/immortalwrt.git $IMMORTALWRT_FOLDER
-#git clone --single-branch https://git.openwrt.org/openwrt/openwrt.git $OPENWRT_FOLDER
+git clone -b openwrt-main --single-branch https://github.com/openwrt/openwrt.git $OPENWRT_FOLDER
+git clone -b immortalwrt-main --single-branch https://github.com/immortalwrt/immortalwrt.git $IMMORTALWRT_FOLDER
 
 cd $OPENWRT_FOLDER
 echo "Current OpenWRT commit"
